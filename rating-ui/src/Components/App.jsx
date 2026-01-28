7dd69792e885b14a546aa5ad80a4605bc83345af
Jonathan De Alba <jdealba15@yahoo.com>
2026-01-27 21:32:18 -0800

    components

---
+++ rating-ui/src/Components/App.jsx
@@ -0,0 +1,14 @@
+const Rating = () => {
+  return (<div>Rate Your Experience</div>)
+}
+const App = () => {
+  return (
+    <div>
+      <Rating />
+      <Rating />
+      <Rating />
+    </div>
+  );
+};
+ 
+export default App;
---
+++ rating-ui/src/Components/Rating.jsx
@@ -0,0 +1,9 @@
+const Rating = () => {
+    return (
+        <div>
+            <h2>Rate Your Expereince</h2>
+        </div>
+    );
+};
+
+export default Rating;
