032758f27a1bb21f676bef705672a6e4ff770ec1
Jonathan De Alba <jdealba15@yahoo.com>
2026-02-08 17:00:23 -0800

    React Event Handler

--- rating-ui/src/Components/Rating.jsx
+++ rating-ui/src/Components/Rating.jsx
@@ -1,10 +1,13 @@
 const Rating = () => {
+    const stars = Array.from({ length: 5 }, (_, i) => i + 1);
+
+    const clicked = () => console.log('Clicked!');
     return (
         <div className='rating-container'>
             <h2>Rate Your Expereince</h2>
             <div className="stars">
-                { [1, 2, 3, 4, 5].map((star, index) => (
-                    <span key={star} className='star'>
+                { stars.map((star) => (
+                    <span onClick={clicked} key={star} className='star'>
                         {'\u2605'}</span>
                 )) }
             </div>
